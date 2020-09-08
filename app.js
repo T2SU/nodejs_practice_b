c08690d0f4812a40f98c1b653fa7c89d87b1eb1f
TSU <tsy@TSU.local>
2020-09-08 21:59:12 +0900

    add files

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!');
-});
-
-app.get('/hello', (req, res) => {
-    res.send('HiHi!! :)');
-});
+app.use(express.static('files'));
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
